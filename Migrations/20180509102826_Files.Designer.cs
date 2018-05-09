110c79bdc0dff9754ee24a949ba989ae36727e2e
Mo <info@norant.be>
2018-05-09 12:31:40 +0200

    profielfotot verwijderd

--- Migrations/20180509102826_Files.Designer.cs
+++ Migrations/20180509102826_Files.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace AngularSPAWebAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180509102826_Files")]
+    partial class Files
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20180509102826_Files.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AngularSPAWebAPI.Migrations
+{
+    public partial class Files : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    FileID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Create = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Omschrijving = table.Column<string>(nullable: true),
+                    OogstkaartItemID = table.Column<int>(nullable: true),
+                    URI = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.FileID);
+                    table.ForeignKey(
+                        name: "FK_Files_OogstkaartItems_OogstkaartItemID",
+                        column: x => x.OogstkaartItemID,
+                        principalTable: "OogstkaartItems",
+                        principalColumn: "OogstkaartItemID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_OogstkaartItemID",
+                table: "Files",
+                column: "OogstkaartItemID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Files");
+        }
+    }
+}
